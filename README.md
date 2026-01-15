bfb92c3a1c67c194fffaace688a0e0bcea2c267e
repserc-ico <kameshima@std.it-college.ac.jp>
2026-01-15 12:07:14 +0900

    1/15 samples

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Practical Network 2025
+
+参考URL：
+https://www.cc.kyoto-su.ac.jp/~mmina/bp1/hundredKnocksBasic.html
---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+echo "Hello, World!"
+
+
+#	pythonでは：
+#	print("Hello, World!")
+
+#	C言語では：
+#	include <stdio.h>
+#	printf("Hello, World!");
+
+#	C++言語では：
+#	include <iostream>
+#	cout << "Hello, World!";
