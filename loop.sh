e69a92fb8371a25a61dbacd9d0b8e4991615593a
repserc-ico <kameshima@std.it-college.ac.jp>
2026-01-20 10:55:07 +0900

    1/20 更新

--- loop.sh
+++ loop.sh
@@ -4,3 +4,13 @@ for i in {1..10}
 do 
 	echo "Hello, World !"
 done
+
+#Pythonだとこう：
+#for i in range(11):
+#	echo "Hello, World !"
+
+#C言語だとこう：
+#include <stdio.h>
+#for (i=1; i<10; i++) {
+#	printf("Hello, World !");
+#}
